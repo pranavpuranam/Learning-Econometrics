3794b09ad3ec53b9a5c3e27e16013872d8e9f94b
Pranav Puranam <puranampranav6@gmail.com>
2026-07-14 19:04:23 +0800

    UPDATE
    Completed RCT.

--- 01_rct.py
+++ 01_rct.py
@@ -15,3 +15,13 @@ means = (
 ) # we compute the average math and reading scores for different class sizes
 
 print(means) # average reading and math scores are higher for students in smaller classes
+
+##################################################
+# Findings
+# Employing smaller class sizes in kindergarten creates a positive effect on student performance in reading and math.
+
+# Lessons
+# 1. The experimental method is what creates an RCT, not shuffling or sampling of the data itself.
+# 2. Before testing causal hypothesis, check for balance in the treatment and control groups.
+# 3. A t-test or ANOVA test (with p-value) can be used to evaluate significance of causal findings.
+##################################################
